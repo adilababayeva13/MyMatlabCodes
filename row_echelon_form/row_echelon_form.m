61f6592e637a1fdadd4078aceca3960f62f31e11
adilababyeva13 <adilababayeva13@gmail.com>
2022-03-11 03:14:16 +0400

    function is created

--- row_echelon_form/row_echelon_form.m
+++ row_echelon_form/row_echelon_form.m
@@ -1,2 +1,51 @@
-matrix = input("Enter the matrix : ");
-fprintf("your matrix is %d\n",matrix);
+A = input("Enter the matrix : ");
+fprintf("Your matrix is :\n");
+disp(A);
+n = size(A,1);
+if n==1
+    fprintf("This is row vector, there is no row echelon form :( \n");
+    return;
+end;
+col=size(A,2);
+A = sortrows(A);
+i=1;k=1;
+while A(i,1)==0
+  row=A(i+1,:);
+  i=i+1;
+end;
+while A(k,1)==0
+    A(k,:)=A(k,:)+row;
+    k=k+1;
+end; 
+A = sortrows(A);
+if col>n
+    for l=1:n
+        s=l;
+        y=A(l,l);
+        while A(l,s)==0
+            y=A(l,s+1);
+            s=s+1;
+        end;
+        A(l,:)=A(l,:)./y;
+        for j=l+1:n
+            x=A(j,l);
+            A(j,:)=A(j,:)-A(l,:)*x;
+        end;
+    end;
+else
+    for l=1:col
+        s=l;
+        y=A(l,l);
+        while A(l,s)==0
+            y=A(l,s+1);
+            s=s+1;
+        end;
+        A(l,:)=A(l,:)./y;
+        for j=l+1:n
+            x=A(j,l);
+            A(j,:)=A(j,:)-A(l,:)*x;
+        end;
+    end;
+end;
+disp(A);
+
