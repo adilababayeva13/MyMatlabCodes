c9268ebc79f7b71f847a3f05c4584efde089570a
adilababyeva13 <adilababayeva13@gmail.com>
2022-03-11 10:30:55 +0400

    inputs are ready

---
+++ newton-raphson_nonlinear-system/newton_raphson.m
@@ -0,0 +1,6 @@
+funcs = input("Enter functions as column vector: ");
+J = input("Enter differentials of the functions as matrix (Jacobian): ");
+initial=input("Enter initial values as column vector: ");
+iter=input("The number of iteration :");
+
+
